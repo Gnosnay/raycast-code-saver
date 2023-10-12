fe68c2c24d573cd5376b85dfd75ae23ac30e2ac3
Yansongsongsong <yansongsongsong@gmail.com>
2023-10-13 01:16:25 +0800

    feat: init status checking logic

--- src/components/init-wrapper.tsx
+++ src/components/init-wrapper.tsx
@@ -7,27 +7,34 @@ export interface InitWrapperProps {
   children: React.ReactNode;
 }
 
-type CheckingStatus = '⌛' | '✅' | '🚨'
-type InitStatus = 'ongoing' | 'allpass' | 'failed'
+// '❓' waiting for execution
+// '⌛' during execution
+// '✅' executed successfully
+// '🚨' something wrong when it is executed
+type CheckingStatus = '❓' | '⌛' | '✅' | '🚨';
+type InitStatus = 'ongoing' | 'allpass' | 'failed';
 
 export const InitWrapper: FC<InitWrapperProps> = ({ children }) => {
-  // TODO use detail to give one spining effects
   const [initChecks, setInitChecks] = useState<[CheckingStatus, string][]>([]);
-  const [initStatus, setInitStatus] = useState<InitStatus>('ongoing');
+  const initStatusSet = new Set<CheckingStatus>(initChecks.map(e => e[0]));
+  const initStatus: InitStatus = initStatusSet.has('🚨') ? 'failed' : (
+    initStatusSet.has('❓') || initStatusSet.has('⌛') ? 'ongoing' : 'allpass'
+  )
 
   setTimeout(() => {
     setInitChecks([...initChecks, ['⌛', 'zxc']])
   }, 1000)
 
-  const checkingPrompt = `## Trying to initilize the application
+  const checkingPrompt = `## Trying to synchronize the application
 ${initChecks.map(([status, prompt]) => `- ${status} ${prompt}`).join("\n")}
 `
   return initStatus != 'allpass' ? <Detail
     navigationTitle="Failed to initialize your snippets store"
     markdown={checkingPrompt}
+    isLoading={initStatus == 'ongoing' ? true : false}
     actions={
       <ActionPanel>
-        <Action title="Change Checking" onAction={() => { setInitStatus('allpass') }} />
+        <Action title="Change Checking" onAction={() => { console.log("checking") }} />
       </ActionPanel>
     }
   /> : <>{children}</>
